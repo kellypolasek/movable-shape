d79a29890ec97f774e7327b0b1db17fc8a0bf85d
kellypolasek <fx2446@wayne.edu>
2016-02-11 20:36:55 -0500

    Movable-car
    Made the car move horizontally by employing movable shape.

--- sketch.js
+++ sketch.js
@@ -6,10 +6,19 @@ var myShape = movableShape;
 // (Basically, you can copy and paste your code from last time, although you may wish to refactor it so that it's more readable.)
 // That said, you may wish to try putting your unrefactored, raw drawing code from week 1 in here
 myShape.drawShape = function () {
+	ellipse(250, 400, 350, 100); // draw the car body
+  ellipse(100, 450, 50, 50); // draw back wheel
+  ellipse(100, 450, 30, 30); // draw back hubcap
+  ellipse(400, 450, 50, 50); // draw front wheel
+  ellipse(400, 450, 30, 30); // draw back hubcap
+  ellipse(431, 397, 10, 20); // draw headlight
+  rect(270, 360, 60, 75); // draw front door
+  rect(170, 360, 60, 75); // draw back door
+  ellipse(391, 351, 5, 45); // draw windshield
 	// your code goes here
 };
 
-// You will also need to do some work to set the speed of the shape. How do you want it to move? What do you need to change to get it to move?
+	myShape.speed.x = 1; // You will also need to do some work to set the speed of the shape. How do you want it to move? What do you need to change to get it to move?
 
 setup = function() {
 	createCanvas(600, 600);
